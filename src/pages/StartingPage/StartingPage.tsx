9da51af3ec394f394b00f2db7ba5d932a4fd2d37
Tomas-David <ttomikdavid@gmail.com>
2024-10-24 20:02:21 +0200

    Raining animation

--- src/pages/StartingPage/StartingPage.tsx
+++ src/pages/StartingPage/StartingPage.tsx
@@ -1,12 +1,16 @@
 import GameLink from "../../components/GameLink/GameLink";
 import classes from "./StartingPage.module.css";
-
+import img from "../../assets/rain.png";
 
 const StartingPage = () => {
   return (
     <div className={classes.container} >
+    <img src={img} alt="rain" className={classes.img}/>
+    <img src={img} alt="rain" className={classes.img_delai}/>
+
     <div className={classes.background}>
     <h1 className={classes.heading}>John Wick The Game</h1>
+    
     <GameLink title="Play" route="/Game"/>
     </div>
         
